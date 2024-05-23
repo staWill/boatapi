5322041be7d7963aeb942f93a53deb9cb5d42e34
StampfliWilliam <william.stampfli@inventx.ch>
2024-05-23 13:43:33 +0200

    feat(persistenz): add boat entity set up flyway and migrations

---
+++ src/main/java/ch/boatapi/repository/boat/Boat.java
@@ -0,0 +1,43 @@
+package ch.boatapi.repository.boat;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "boat")
+public class Boat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "description", nullable = false)
+    private String description;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
