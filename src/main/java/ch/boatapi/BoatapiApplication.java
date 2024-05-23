2611e94a4f93944bed4ab6510d3f938cd8d922bb
StampfliWilliam <william.stampfli@inventx.ch>
2024-05-23 13:42:36 +0200

    init

---
+++ src/main/java/ch/boatapi/BoatapiApplication.java
@@ -0,0 +1,13 @@
+package ch.boatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoatapiApplication.class, args);
+    }
+
+}
