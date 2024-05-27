de302ac199df837a0b03c53eb3fd2ec52805bd94
StampfliWilliam <william.stampfli@inventx.ch>
2024-05-27 17:06:28 +0200

    update local keycloack and add cors for ui integration

--- src/main/java/ch/boatapi/BoatapiApplicationConfig.java
+++ src/main/java/ch/boatapi/BoatapiApplicationConfig.java
@@ -3,12 +3,28 @@ package ch.boatapi;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class BoatapiApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
     }
 
 }
